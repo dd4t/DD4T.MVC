d55e161f14f555a77a3a5d630f0429669300865d
quirijnslings <quirijn.slings@trivident.com>
2019-12-20 17:40:34 +0100

    fixed bug by removing trailing slash from the request when rewriting it

--- source/DD4T.Web/Binaries/BinaryDistributionModule.cs
+++ source/DD4T.Web/Binaries/BinaryDistributionModule.cs
@@ -104,15 +104,21 @@ namespace DD4T.Web.Binaries
                 return;
             }
 
-            string realPath = request.PhysicalApplicationPath + "BinaryData" + request.Path.Replace("/", "\\"); // request.PhysicalPath;
-            context.RewritePath("/BinaryData" + request.Path);
+            string realPath = request.PhysicalApplicationPath + "BinaryData" + request.Path.Replace('/', Path.DirectorySeparatorChar); // request.PhysicalPath;
+            realPath = realPath.TrimEnd(new[] { Path.DirectorySeparatorChar });
+            if (!request.Path.ToLower().StartsWith("/binarydata"))
+            {
+                context.RewritePath("/BinaryData" + request.Path.TrimEnd('/'));
+            }
 
             if (!File.Exists(realPath))
             {
-                _logger.Debug("Dir path: " + realPath.Substring(0, realPath.LastIndexOf("\\")));
+                var dir = Path.GetDirectoryName(realPath);
+                _logger.Debug("Dir path: " + dir);
+                _logger.Debug("Dir path the old way: " + realPath.Substring(0, realPath.LastIndexOf("\\")));
                 try
                 {
-                    string dir = realPath.Substring(0, realPath.LastIndexOf("\\"));
+                    //string dir = realPath.Substring(0, realPath.LastIndexOf("\\"));
 
                     if (!Directory.Exists(dir))
                         Directory.CreateDirectory(dir);
